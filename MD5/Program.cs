ce031cce7ffa16102f026ead61b0463588973b9c
Spatenheinz <jacob@1362.dk>
2021-04-23 14:24:12 +0200

    why is it not working

--- MD5/Program.cs
+++ MD5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SME;
-using MD5.opt1;
+using System;
+// using MD5.opt1;
 
 namespace MD5
 {
@@ -8,39 +9,42 @@ namespace MD5
         static void Main(string[] args)
         {
             using (var sim = new Simulation()) {
-                // var md5 = new MD5();
+                var md5 = new MD5();
                 // Nice to be able to test buffer sizes
                 // Console.WriteLine(str);
-                var tester = new Tester();
+                var tester = new Tester("");
                 // opt1
-                var formatter = new MessageFormat();
-                formatter.Message = tester.Message;
-                var converter = new FormatConverter();
-                converter.paddedBuffer = formatter.paddedBuffer;
-                var roundF    = new RoundF();
-                roundF.block = converter.block;
-                roundF.IV    = tester.optDigest;
-                var roundG    = new RoundG();
-                roundG.F     = roundF.Out;
-                roundG.block = converter.block;
-                var roundH    = new RoundH();
-                roundH.G     = roundG.Out;
-                roundH.block = converter.block;
-                var roundI    = new RoundI();
-                roundI.H     = roundH.Out;
-                roundI.block = converter.block;
-                var combinator = new Combiner();
-                combinator.I = roundI.Out;
-                combinator.IV = tester.optDigest;
-                tester.Digest2 = combinator.Out;
+                // var formatter = new MessageFormat();
+                // formatter.Message = tester.Message;
+                // var converter = new FormatConverter();
+                // converter.paddedBuffer = formatter.paddedBuffer;
+                // var roundF    = new RoundF();
+                // roundF.block = converter.block;
+                // roundF.IV    = tester.optDigest;
+                // var roundG    = new RoundG();
+                // roundG.F     = roundF.Out;
+                // roundG.block = converter.block;
+                // var roundH    = new RoundH();
+                // roundH.G     = roundG.Out;
+                // roundH.block = converter.block;
+                // var roundI    = new RoundI();
+                // roundI.H     = roundH.Out;
+                // roundI.block = converter.block;
+                // var combinator = new Combiner();
+                // combinator.I = roundI.Out;
+                // combinator.IV = tester.optDigest;
+                // tester.Digest2 = combinator.Out;
                 //
 
-                // md5.Message = tester.Message;
-                // tester.Digest = md5.Digest;
-                    // sim.AddTopLevelInputs(md5.Message)
-                    //     .AddTopLevelOutputs(md5.Digest)
-                        sim.AddTopLevelInputs(tester.optDigest, tester.Message)
-                       .AddTopLevelOutputs(combinator.Out)
+                md5.Message = tester.Message;
+                md5.out_o = tester.res_o;
+                tester.Digest = md5.Digest;
+                tester.in_o = md5.in_o;
+                    sim.AddTopLevelInputs(md5.Message, md5.out_o)
+                        .AddTopLevelOutputs(md5.Digest, md5.in_o)
+                       //  sim.AddTopLevelInputs(tester.optDigest, tester.Message)
+                       // .AddTopLevelOutputs(combinator.Out)
+                        .AddTicker(s => Console.WriteLine($"Ticks {Scope.Current.Clock.Ticks}"))
                         .BuildCSVFile()
                         .BuildGraph()
                         .BuildVHDL()
--- MD5/Tester.cs
+++ MD5/Tester.cs
@@ -9,23 +9,22 @@ namespace MD5
 {
     public class Tester : SimulationProcess
     {
+        [InputBus] public IDigest Digest;
+        [OutputBus] public Iaxis_o res_o = Scope.CreateBus<Iaxis_o>();
+
         // [InputBus]
-        // public IDigest Digest;
+        // public IRound Digest2;
 
-        [InputBus]
-        public IRound Digest2;
-
-        [OutputBus]
-        public IRound optDigest = Scope.CreateBus<IRound>();
-        [OutputBus]
-        public IMessage Message = Scope.CreateBus<IMessage>();
+        // [OutputBus]
+        // public IRound optDigest = Scope.CreateBus<IRound>();
+        [OutputBus] public IMessage Message = Scope.CreateBus<IMessage>();
+        [InputBus] public Iaxis_o in_o;
 
         private readonly string[] MESSAGES;
 
-        private static int testsize = 1;
+        private static int testsize = 10;
         private string[] randomStrings = new string[testsize];
         private static Random random = new Random();
-
 
         private System.Security.Cryptography.MD5 Target = System.Security.Cryptography.MD5.Create();
 
@@ -35,13 +34,82 @@ namespace MD5
             if (messages.Length == 0) {
                 for (int i = 0; i < testsize; i++) {
                     randomStrings[i] = RandomString((i+1) * 2000);
-
                 }
                 MESSAGES = randomStrings;
             } else { MESSAGES = messages; }
-
         }
 
+
+        public async override Task Run() {
+
+            await ClockAsync();
+            foreach (string message in MESSAGES) {
+                // optDigest.A = 0x67452301;
+                // optDigest.B = 0xefcdab89;
+                // optDigest.C = 0x98badcfe;
+                // optDigest.D = 0x10325476;
+                int counter = 0;
+                int buffersize = 0;
+                int offset = 0;
+                Message.MessageSize = message.Length;
+                Message.Head = true;
+                int i = message.Length;
+                while (i >= 0) {
+                // optDigest.Valid = true;
+                    offset = MAX_BUFFER_SIZE;
+                    if (i < 56)
+                    {
+                        Message.BufferSize = buffersize = i;
+                        Message.Last = true;
+                        i -= offset;
+                    }
+                    else
+                    {   offset = Math.Min(i, MAX_BUFFER_SIZE);
+                        Message.Set = offset < MAX_BUFFER_SIZE;
+                        Message.BufferSize = buffersize = offset;
+                        Message.Last = false;
+                        i -= offset;
+                    }
+                    for(int j = 0 ; j < MAX_BUFFER_SIZE; j++)
+                        if (j < buffersize)
+                        {
+                            Message.Message[j] = (byte)message[MAX_BUFFER_SIZE * counter + j];
+                        } else {
+                            Message.Message[j] = 0;
+                        }
+                    if (counter++ > 0) { Message.Head = false; }
+                    Message.Valid = true;
+                        await ClockAsync();
+                        // Digest2.Valid = false;
+                        Message.Valid = false;
+                    // optDigest.A = Digest2.A;
+                    // optDigest.B = Digest2.B;
+                    // optDigest.C = Digest2.C;
+                    // optDigest.D = Digest2.D;
+                    // Digest2.Valid = false;
+                    // Message.Valid = false;
+                }
+                res_o.Ready = true;
+                await ClockAsync();
+                if (Digest.Valid) {
+                string str = "";
+                // string str2 = "";
+                // str2 += reverseByte(optDigest.A).ToString("X8");
+                // str2 += reverseByte(optDigest.B).ToString("X8");
+                // str2 += reverseByte(optDigest.C).ToString("X8");
+                // str2 += reverseByte(optDigest.D).ToString("X8");
+                for(int j = 0; j < 4; j++) {
+                    str += Digest.Digest[j].ToString("X8");
+                }
+                Debug.Assert(str == targetHash(message), $"String {message} with Hash nr. {0} - {str} doesnt match the MS library {targetHash(message)}");
+                // Debug.Assert(str2 == targetHash(message), $"String2 {message} with Hash nr. {0} - {str2} doesnt match the MS library {targetHash(message)}");
+                Console.WriteLine(str);
+                }
+            }
+            // res_o.Ready = false;
+            // Debug.Assert(Digest.Valid && optDigest.Valid, "failed to produce any output");
+            // await ClockAsync();
+        }
         private uint reverseByte(uint i) {
             return ((i & 0x000000ff) << 24) |
                 (i >> 24) |
@@ -59,77 +127,6 @@ namespace MD5
         private string targetHash(string message) {
             byte[] target = Target.ComputeHash(System.Text.Encoding.UTF8.GetBytes(message));
             return BitConverter.ToString(target).Replace("-", string.Empty);
-        }
-
-        public async override Task Run() {
-
-            await ClockAsync();
-            foreach (string message in MESSAGES) {
-                optDigest.A = 0x67452301;
-                optDigest.B = 0xefcdab89;
-                optDigest.C = 0x98badcfe;
-                optDigest.D = 0x10325476;
-                int counter = 0;
-                Message.MessageSize = message.Length;
-                Message.Head = true;
-
-                int i = message.Length;
-                while (i >= 0) {
-                optDigest.Valid = true;
-                    int offset = MAX_BUFFER_SIZE;
-                    if (i < 56)
-                    {
-                        Message.BufferSize = i;
-                        Message.Last = true;
-                        i -= offset;
-                    }
-                    else
-                    {   offset = Math.Min(i, MAX_BUFFER_SIZE);
-                        Message.Set = offset < MAX_BUFFER_SIZE;
-                        Message.BufferSize = offset;
-                        Message.Last = false;
-                        i -= offset;
-                    }
-                    await ClockAsync();
-                    for(int j = 0 ; j < MAX_BUFFER_SIZE; j++)
-                        if (j < Message.BufferSize)
-                        {
-                            Message.Message[j] = (byte)message[MAX_BUFFER_SIZE * counter + j];
-                        } else {
-                            Message.Message[j] = 0;
-                        }
-                    if (counter++ > 0) { Message.Head = false; }
-                    Message.Valid = true;
-                    // while (optDigest.Valid)
-                        // Console.WriteLine("hey");
-                        await ClockAsync();
-                        // Console.WriteLine("i am here");
-                        // Console.WriteLine($"called cob Out: {Digest2.A}, {Digest2.B}, {Digest2.C}, {Digest2.D}");
-                        // Digest2.Valid = false;
-                        // Message.Valid = false;
-                    optDigest.A = Digest2.A;
-                    optDigest.B = Digest2.B;
-                    optDigest.C = Digest2.C;
-                    optDigest.D = Digest2.D;
-                    Digest2.Valid = false;
-                    Message.Valid = false;
-                }
-                await ClockAsync();
-                // string str = "";
-                string str2 = "";
-                str2 += reverseByte(optDigest.A).ToString("X8");
-                str2 += reverseByte(optDigest.B).ToString("X8");
-                str2 += reverseByte(optDigest.C).ToString("X8");
-                str2 += reverseByte(optDigest.D).ToString("X8");
-                // for(int j = 0; j < 4; j++) {
-                //     str += Digest.Digest[j].ToString("X8");
-                // }
-                // Debug.Assert(str == targetHash(message), $"String {message} with Hash nr. {0} - {str} doesnt match the MS library {targetHash(message)}");
-                Debug.Assert(str2 == targetHash(message), $"String2 {message} with Hash nr. {0} - {str2} doesnt match the MS library {targetHash(message)}");
-            }
-
-            // Debug.Assert(Digest.Valid && optDigest.Valid, "failed to produce any output");
-            // await ClockAsync();
         }
     }
 }
